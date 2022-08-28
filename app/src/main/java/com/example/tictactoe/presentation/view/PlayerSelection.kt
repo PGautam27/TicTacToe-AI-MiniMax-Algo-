ce2ec3e9b4e4c38ae58c473d7f0edf90fcee34c0
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-28 19:05:39 +0530

    AI - v1

---
+++ app/src/main/java/com/example/tictactoe/presentation/view/PlayerSelection.kt
@@ -0,0 +1,29 @@
+package com.example.tictactoe.presentation.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PlayerSelection() {
+    val player = remember {
+        mutableStateOf(String())
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Choose What you wanna be")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "X")
+        }
+    }
+}
